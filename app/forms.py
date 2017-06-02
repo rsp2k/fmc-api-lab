2de16af335430e1da5147fd8d0bc8a2dddaac39d
Ryan Malloy <ryan@supported.systems>
2017-06-01 18:51:29 -0600

    add lifetime

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, IPAddress, NumberRange
 
 class PinholeRequestForm(FlaskForm):
     ip = StringField(u'Destination address', validators=[DataRequired(), IPAddress(message="Enter a valid IP")])
-    port = IntegerField(u'Destination Port', validators=[DataRequired(), NumberRange(min=1,max=65536)])
+    port = IntegerField(u'Destination Port', validators=[DataRequired(), NumberRange(min=1024,max=65536)])
+    lifetime = IntegerField(u'Lifetime (in seconds)', validators=[DataRequired(), NumberRange(min=300,max=3600)
 
     submit = SubmitField(u'Create Pinhole')
